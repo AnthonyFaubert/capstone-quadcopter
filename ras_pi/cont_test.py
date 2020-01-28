5c18f05f1a5b1ef3b92c7544adc37f927aaee647
Griffin <griffinhardy13@gmail.com>
2020-01-28 15:37:29 -0800

    added missing comma

--- ras_pi/cont_test.py
+++ ras_pi/cont_test.py
@@ -4,6 +4,6 @@ import subprocess
 sc_proc = subprocess.Popen('sc-controller', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 time.sleep(5)
 sc_proc.kill()
-js_proc = subprocess.Popen('jstest-gtk' stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+js_proc = subprocess.Popen('jstest-gtk', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 time.sleep(3)
 js_proc.kill()
