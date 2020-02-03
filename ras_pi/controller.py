2c7734cdc8824881822e8b844d66ec7840aff707
Griffin <griffinhardy13@gmail.com>
2020-02-03 14:58:03 -0800

    controller update

--- ras_pi/controller.py
+++ ras_pi/controller.py
@@ -4,6 +4,8 @@ import serial
 import subprocess
 import struct
 
+DATA_LENGTH = 10
+
 # open the steam controller driver
 sc_proc = subprocess.Popen('sc-controller', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 time.sleep(1)
@@ -26,7 +28,8 @@ clock = pygame.time.Clock()
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
-ser = serial.Serial(port = "/dev/ttyS0", baudrate = 115200, write_timeout = 0)
+ser = serial.Serial(port = "/dev/ttyS0", baudrate = 115200, write_timeout = None, timeout = 0) #read timeout should get set to None
+special = 0
 
 def hexify(bs):
     return ''.join('{:02x}'.format(x) for x in bs)
@@ -43,29 +46,74 @@ def get_yt(): # right trackpad used as stick, axes 3 and 4. Axis 4 is up/down an
 
 def send_data(dataValue):
     ser.write(dataValue)
+    ser.write(dataValue)
     print(hexify(dataValue))
+    print()
     
+ser.reset_input_buffer()
+ser.reset_output_buffer()
 while not done:
     clock.tick(1) #integer value is the frames per second
     
     for event in pygame.event.get(): # User did something.
         if event.type == pygame.QUIT: # If user clicked close.
             done = True # Flag that we are done so we exit this loop.
-    
+        elif event.type == pygame.JOYBUTTONDOWN:
+            if (event.button == 6 or event.button == 7):
+                special = 5
+            elif (event.button == 0):
+                special = 1
+            elif (event.button == 1):
+                special = 2
+            elif (event.button == 2):
+                special = 3
+            elif (event.button == 3):
+                special = 4
+
+#        elif (event.type == pygame.JOYAXISMOTION and (event.axis == 6 or event.axis == 7)):
+#            print("axis 6 is " + str(joystick.get_axis(6)))
+#            print("axis 7 is " + str(joystick.get_axis(7)))
+#            if joystick.get_axis(6) < -0.1:
+#                special = 1
+#            elif joystick.get_axis(6) > 0.1:
+#                special = 2
+#            elif joystick.get_axis(7) < -0.1:
+#                special = 3
+#            elif joystick.get_axis(7) > 0.1:
+#                special = 4
+
+#    if (joystick.get_axis(6) > 0.1):
+#        special = 1
+#    elif (joystick.get_axis(6) < -0.1):
+#        special = 2
+#    elif (joystick.get_axis(7) > 0.1):
+#        special = 3
+#    elif (joystick.get_axis(7) < -0.1):
+#        special = 4
+
+
+    print("value of special is: " + str(special))
+
     if ready_to_send:
         tilt = get_tilt()
-        print("tilt lr = " + str(tilt[0]))
         yt = get_yt()
-        ready_to_send = True #needs to be set to False eventually
-        data = struct.pack('<hhhh', tilt[0], tilt[1], yt[0], yt[1])
+        ready_to_send = True  #needs to be set to False eventually
+        data = struct.pack('>hhhhh', tilt[0], tilt[1], yt[0], yt[1], special)
+        print("below is sent data:")
         send_data(data)
-        print("sent one set")
+        print("ser out waiting " + str(ser.out_waiting))
+        print()
+        time.sleep(1)
+        special = 0
     
     if (ser.in_waiting > 0):
+        print("below is bytes waiting")
+        print(ser.in_waiting)
         ser.ready_to_send = True
+        print("****************below is read data:") 
+        print(hexify(ser.read(DATA_LENGTH)))
+        print()
         ser.reset_input_buffer()
-
-    print("waiting for David signal")
 
 #turn off motors
 ser.close()
--- ras_pi/sertest.py
+++ ras_pi/sertest.py
@@ -5,12 +5,29 @@
 
 import time 
 import serial
-ser = serial.Serial(port = "/dev/ttyS0", baudrate = 9600, write_timeout = 0, timeout = 0)
-time.sleep(5)
-ser.write(1000)
-print(ser.in_waiting)
-print("Just slept for 5 seconds.")
-print("about to close serial connection")
+import struct 
+
+ser = serial.Serial(port = "/dev/ttyS0", baudrate = 115200, write_timeout = None, timeout = None)
+
+
+totBytes = 8
+delay = 0
+
+x = 1
+ser.reset_input_buffer()
+
+while(True):
+#    val = struct.pack(b, x)
+    val = x.to_bytes(totBytes, byteorder='big')
+    ser.write(val)
+    #ser.write(val)
+    #ser.write(val)
+    time.sleep(delay)
+    print(str(val) + " " + str(ser.read(totBytes)))
+    if (x < 2**(8*totBytes)):
+        x = x + 1
+    else:
+        x = 0
 
 ser.close()
 
