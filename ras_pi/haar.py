5e4d55fe6914d7c82cf43d7f6827a1629ae06a04
Griffin <griffinhardy13@gmail.com>
2020-02-20 14:15:47 -0800

    Updated haar.py to include an initialization phase.
    The initialization phase waits until tit detects a face before entering the main while loop now.

--- ras_pi/haar.py
+++ ras_pi/haar.py
@@ -1,25 +1,34 @@
 import numpy as np
 import cv2
 import time
-from pathlib import Path
 
-cascade_path = Path("/home/pi/serial_interface/capstone-quadcopter/ras_pi/haarcascade_frontalface_default.xml")
-print(cascade_path)
-
-face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_alt.xml')
-casc = face_cascade.empty()
+face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml') #filter can be swapped with ....face_alt.xml
 no_face_frames = 0
-old_center = [319,119]
-old_size = [100, 100]
+#prev_center = [319,119] #arbitrary values that correspond to center of frame
+#prev_size = [100, 100] #arbitrary values for person sitting about 1 foot in front of camera
 
-if (casc):
-    print("no cascade filter found")
+if (face_cascade.empty()):
+    print("No cascade filter found.")
+
 cap = cv2.VideoCapture(0)
+num_faces = 0
+init_frames = 0
     
 if not cap.isOpened():
     print("unable to open camera")
+else:
+    while(num_faces == 0):
+        ret, img = cap.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces, num_faces = face_cascade.detectMultiScale2(gray, 1.8, 3)
+        init_frames = init_frames + 1
+
+print("The total number of intialization frames before a face was found was " + str(init_frames))
+
+prev_center = [faces[0][0]+(0.5*faces[0][2]), faces[0][1]+(0.5*faces[0][3])]
+prev_size = [faces[0][2], faces[0][3]]
     
-while(cap.isOpened() and (not casc)):
+while(cap.isOpened() and (not face_cascade.empty())):
     start_cap = time.time()
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -43,18 +52,18 @@ while(cap.isOpened() and (not casc)):
         print('y of bounding: ' + str(y))
         print('width: ' + str(w))
         print('height: ' + str(h))
-        if (center[0] < (old_center[0]+50) and center[0] > (old_center[0]-50)
-            and center[1] < (old_center[1]+50) and center[1] > (old_center[1]-50)
-            and size[0] < (old_size[0]*2) and size[0] > (old_size[0]*0.5) 
-            and size[1] < (old_size[1]*2) and size[1] > (old_size[1]*0.5)):
-            old_center = [x + (0.5*w), y + (0.5*h)]
+        if (center[0] < (prev_center[0]+50) and center[0] > (prev_center[0]-50)
+            and center[1] < (prev_center[1]+50) and center[1] > (prev_center[1]-50)
+            and size[0] < (prev_size[0]*2) and size[0] > (prev_size[0]*0.5)
+            and size[1] < (prev_size[1]*2) and size[1] > (prev_size[1]*0.5)):
+            prev_center = [x + (0.5*w), y + (0.5*h)]
             img = cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
-            old_size = [w, h]
+            prev_size = [w, h]
         face_det = True
 
-    if (not face_det):
+    if (num_faces == 0):
         no_face_frames = no_face_frames + 1
-        print("Number of frames with no face is " + str(no_face_frames) + ".\n")
+        print("Number of frames with no face is " + str(no_face_frames) + ".")
 
     end_calc = time.time()
     delta_calc = (end_calc - start_calc) * 1000
@@ -65,7 +74,6 @@ while(cap.isOpened() and (not casc)):
     end_show = time.time()
     delta_show = (end_show - start_show) * 1000
     print("%.2f ms to show image" %delta_show)
-    #cv2.waitKey(0)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
