72a7585f7422212a2698ca88479d44cfd463b80f
Anthony Faubert <anthonydfaubert@gmail.com>
2020-03-01 16:47:10 -0800

    Fixed a few minor bugs.

--- quadcontrol/Inc/Experiments.h
+++ quadcontrol/Inc/Experiments.h
@@ -2,11 +2,18 @@
 #ifndef __EXPERIMENTS_H__
 #define __EXPERIMENTS_H__
 
+#include "stdint.h"
+
+// For experiments to e-stop when done
+extern void emergencyStop();
+
 // For confirming the motor mapping is correct.
 // Turns on the motors at low values each for 2.5 seconds in a pattern (motor 0, then 1, 2, & 3).
 void experiment_CheckMotorMap(uint32_t logTimestamp, float* mVals);
 
+
 // These experiments require a swing jig //
+
 // For determining good initial PD gains.
 // Turns on the motors to a baseline level, waits for the operator to stop any swinging, then applies a sine wave command for several seconds.
 void experiment_SineWavePitch(uint32_t logTimestamp, float* mVals, float* thrust);
