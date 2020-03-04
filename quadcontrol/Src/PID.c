a4bf749a8e3c852580a682af433a28b779c22d5b
Anthony Faubert <anthonydfaubert@gmail.com>
2020-03-03 19:20:49 -0800

    Added Quaternion to Euler conversion math from Wikipedia.

--- quadcontrol/Src/PID.c
+++ quadcontrol/Src/PID.c
@@ -19,7 +19,30 @@ void ApplyGyroCorrection(GyroData* gyroData) {
 
 // Converts a quaternion rotation to an Euler rotation representation
 RollPitchYaw Quaternion2Euler(Quaternion q) {
-  
+  // Contents of this function are from Wikipedia with minor modifications
+  RollPitchYaw ypr;
+
+  // roll (x-axis rotation)
+  float sinr_cosp = 2.0f * (q.w * q.x + q.y * q.z);
+  float cosr_cosp = 1.0f - 2.0f * (q.x * q.x + q.y * q.y);
+  ypr.roll = atan2f(sinr_cosp, cosr_cosp);
+
+  // pitch (y-axis rotation)
+  float sinp = 2.0f * (q.w * q.y - q.z * q.x);
+  if (sinp > 1.0f) { // use 90 degrees if out of range
+    ypr.pitch = PI / 2.0f;
+  } else if (sinp < -1.0f) {
+    ypr.pitch = -PI / 2.0f;
+  } else {
+    ypr.pitch = asinf(sinp);
+  }
+
+  // yaw (z-axis rotation)
+  float siny_cosp = 2.0f * (q.w * q.z + q.x * q.y);
+  float cosy_cosp = 1.0f - 2.0f * (q.y * q.y + q.z * q.z);
+  ypr.yaw = atan2f(siny_cosp, cosy_cosp);
+
+  return ypr;
 }
 
 // Gives rotation required to get from the actual orientation to the desired orientation
